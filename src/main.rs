540ecb6246b879a5ed34638617943da823d0e3dc
Luxko <liarluxlux@gmail.com>
2017-02-15 23:28:06 +0800

    Hotfix for mdbook

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 //! Building scripts
 use std::process::Command;
-
+use std::path::{Path, PathBuf};
 
 fn main() {
     println!("Building start...");
-    testing();
+    //testing();
     rendering();
     println!("Building complete.");
 }
@@ -20,52 +20,63 @@ fn testing() {
 
 fn rendering() {
     print!("Rendering...");
+    let (olds, news) = before_rendering();
     let mut render_proc = Command::new("mdbook").arg("build").spawn()
                                 .expect("Failed to start the rendering process");
     let ecode = render_proc.wait().expect("Failed to finish the rendering process");
     assert!(ecode.success());
-    post_rendering();
+    post_rendering(olds, news);
     println!("Done.");
 }
 
-fn post_rendering() {
-    let cdir = std::env::current_dir().expect("WTF current directory does not exist");
-    process_dir(cdir, cdir);
-    use std::fs::*;
-    use std::path::Path;
-    const BOOK_DIR_NANE = "book";
-    fn process_dir(path: &Path, root: &Path) {
-        if let Ok(entries) = read_dir(path) {
-            for entry in entries {
-                if let Ok(entry) = entry {
-                    if let Ok(file_type) = entry.file_type() {
-                        if file_type.is_dir() {
-                            let file_path = entry.path();
-                            if let Some(name) = file_path.file_name() {
-                                if name != BOOK_DIR_NAME {
-                                    process_dir(entry.path().as_path(), root);
-                                }
-                            }
-                            
-                        } else if file_type.is_file() {
-                            let file_path = entry.path();
-                            if let Some(ext) = file_path.extension() {
-                                if ext == "html" {
-                                    process_html(file_path.as_path(), root);
-                                }
-                            }
-                        }
-                    }
+fn before_rendering() -> (Vec<PathBuf>, Vec<PathBuf>) {
+    let root = std::env::current_dir().expect("WTF current directory does not exist");
+    let file_paths = {
+            fn file_paths(spath: &Path) -> Vec<String> {
+                use std::io::{Read, Write};
+                let scontent = {
+                let sfile = std::fs::File::open(spath)
+                    .expect("Failed to open SUMMARY.md");
+                let mut buf = String::with_capacity(sfile.metadata().unwrap().len() as usize);
+                (&sfile).read_to_string(&mut buf).expect("Failed to read from SUMMARY.md");
+                buf
+            };
+
+            let mut paths = Vec::new();
+            for line in scontent.lines() {
+                if let Some(index) = line.find("(./") {
+                    let (_, path) = line.split_at(index+3);
+                    paths.push(path[..path.len()-1].to_owned());
                 }
             }
+            paths
         }
+        let mut summary_path = root.to_path_buf();
+        summary_path.push("src");
+        summary_path.push("SUMMARY.md");
+        file_paths(summary_path.as_path())
+    };
+    let mut old_paths = Vec::new();
+    let mut new_paths = Vec::new();
+    for path in &file_paths {
+        let mut old = root.to_path_buf();
+        old.push(&path);
+        let mut new = root.to_path_buf();
+        new.push("src");
+        new.push(&path);
+        std::fs::create_dir_all(new.parent().unwrap())
+            .expect("Failed to move a file");
+        std::fs::rename(old.as_path(), new.as_path())
+            .expect("Failed to move a file");
+        old_paths.push(old);
+        new_paths.push(new);
     }
-    fn process_html(path: &Path, root: &Path) {
-        const BOOK_DIR_NANE = "book";
-        let sub_path = path.strip_prefix(root).expect("invalid call");
-        let mut new_path = root.to_owned();
-        new_path.push(BOOK_DIR_NAME);
-        new_path.push(sub_path);
-        rename(path, new_path).expect("Failed to rename a file");
+    (old_paths, new_paths)
+}
+fn post_rendering(olds: Vec<PathBuf>, news: Vec<PathBuf>) {
+    assert_eq!(olds.len(), news.len());
+    for i in 0..olds.len() {
+        std::fs::rename(news[i].as_path(), olds[i].as_path())
+            .expect("Failed to move back!");
     }
 }
